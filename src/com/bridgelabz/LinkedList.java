523d9703474e19a19de5819f7f70d15459dd2127
9663461085 <ratansollapure16310@gmail.com>
2022-12-11 22:46:52 +0530

    Added:Changed Sequence of linked list by adding last

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -26,14 +26,36 @@ public class LinkedList {
 	 * 
 	 * @param data
 	 */
+	public void elements(Object data) {
+		Node newNode = new Node(data);
+		System.out.print(newNode.data + " ");
+	}
+	/*
+	 * Method to add elements at first
+	 */
 	public void addFirst(Object data) {
 		Node newNode = new Node(data);
 		newNode.reference = head;
 		head = newNode;
 	}
-
 	/*
-	 * Method for displaying the added elements
+	 * Method to add elements at last 
+	 */
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+		Node temp=head;
+		if(head==null)
+			head=newNode;
+		else {
+			
+		while(temp.reference!=null) {
+			temp=temp.reference;
+		}
+		temp.reference=newNode;
+		}
+	}
+	/*
+	 * Method to  displaying the added elements
 	 */
 	public void display() {
 		if (head == null)
@@ -49,15 +71,25 @@ public class LinkedList {
 			}
 		}
 	}
-
+	
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Linked List program");
 		LinkedList list = new LinkedList();
+		System.out.println("Printing nodes at First");
 		list.addFirst(70);
 		list.display();
 		list.addFirst(30);
 		list.display();
 		list.addFirst(56);
+		list.display();
+		System.out.println("-----------------");
+		System.out.println("Printing nodes at last");
+		list.addLast(56);
+		list.display();
+		list.addLast(30);
+		list.display();
+		list.addLast(70);
 		list.display();
 	}
 
